d6102dc121b569caf6ee1d127dac260c7de5e616
Joshua Streger <Joshua.Streger@tradingscreen.com>
2020-08-03 21:10:11 -0400

    Updates

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography";
-import twinPeaksTheme from "typography-theme-twin-peaks";
-
-const typography = new Typography(twinPeaksTheme);
-
-export const { scale, rhythm, options } = typography;
-export default typography;
